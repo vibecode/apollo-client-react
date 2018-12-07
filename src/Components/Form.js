8e8c6645238e4a1ab36c6445592c48a7843766a6
vibecode <rumashkin@gmail.com>
2018-12-07 14:19:59 +0100

    extract form

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,27 +1,52 @@
 import React from 'react'
 
-const Form = ({ title, body, onInputChange, onSubmit }) => {
-  return (
-    <form onSubmit={onSubmit}>
-      <input
-        value={title}
-        type="text"
-        name="title"
-        placeholder="Title"
-        onChange={onInputChange}
-      />
-      <textarea
-        value={body}
-        name="body"
-        id=""
-        cols="30"
-        rows="10"
-        placeholder="body"
-        onChange={onInputChange}
-      />
-      <button>Submit</button>
-    </form>
-  )
+class Form extends React.Component {
+  state = {
+    title: '',
+    body: ''
+  }
+
+  handleInput = ev => {
+    this.setState({ [ev.target.name]: ev.target.value })
+  }
+
+  handleSubmit = async ev => {
+    const { title, body } = this.state
+    ev.preventDefault()
+
+    await this.props.queryCallback({
+      variables: {
+        title,
+        body
+      }
+    })
+
+    this.setState({ title: '', body: '' })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          value={this.state.title}
+          type="text"
+          name="title"
+          placeholder="Title"
+          onChange={this.handleInput}
+        />
+        <textarea
+          value={this.state.body}
+          name="body"
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="body"
+          onChange={this.handleInput}
+        />
+        <button>Submit</button>
+      </form>
+    )
+  }
 }
 
 export default Form
--- src/Components/NewPost.js
+++ src/Components/NewPost.js
@@ -14,57 +14,12 @@ const NEW_POST = gql`
 `
 
 export class NewPost extends PureComponent {
-  constructor(props) {
-    super(props)
-    this.state = { title: '', body: '' }
-  }
-
-  handleInput = ev => {
-    console.log(ev.target.value)
-    this.setState({ [ev.target.name]: ev.target.value })
-  }
-
-  handleSubmit = createPost => {
-    createPost().then(() =>
-      this.setState({
-        title: '',
-        body: ''
-      })
-    )
-  }
-
-  renderForm = createPost => {
-    const { title, body } = this.state
-
-    const handleSubmit = e => {
-      e.preventDefault()
-
-      createPost().then(() =>
-        this.setState({
-          title: '',
-          body: ''
-        })
-      )
-    }
-
-    return (
-      <Form
-        onSubmit={handleSubmit}
-        onInputChange={this.handleInput}
-        title={title}
-        body={body}
-      />
-    )
-  }
-
   render() {
-    const { title, body } = this.state
-
     return (
       <div>
         <h1>New post</h1>
-        <Mutation mutation={NEW_POST} variables={{ title, body }}>
-          {createPost => this.renderForm(createPost)}
+        <Mutation mutation={NEW_POST}>
+          {createPost => <Form queryCallback={createPost} />}
         </Mutation>
       </div>
     )
