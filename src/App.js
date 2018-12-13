5b2fbf6f41aef24e8e9cfd33417f2533f46062c2
vibecode <rumashkin@gmail.com>
2018-12-13 14:45:41 +0100

    add persistent cache

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
 import React, { Component } from 'react'
 import './App.css'
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Posts from './Components/Posts'
 import Post from './Components/Post'
 import NewPost from './Components/NewPost'
 
-const client = new ApolloClient({
-  uri: process.env.REACT_APP_APOLLO_CLIENT_ENDPOINT,
-  clientState: {
-    defaults: { isEditMode: false },
-    resolvers: {}
-  }
-})
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
-        <Router>
-          <div>
-            <Switch>
-              <Route
-                path={'/post/new'}
-                component={NewPost}
-              />
-              <Route path={'/post/:id'} component={Post} />
-              <Route path={'/'} component={Posts} />
-            </Switch>
-          </div>
-        </Router>
-      </ApolloProvider>
+      <Router>
+        <div>
+          <Switch>
+            <Route path={'/post/new'} component={NewPost} />
+            <Route path={'/post/:id'} component={Post} />
+            <Route path={'/'} component={Posts} />
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { persistCache } from 'apollo-cache-persist'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache()
+
+persistCache({
+  cache,
+  storage: window.localStorage
+}).then(() => {
+  const client = new ApolloClient({
+    cache,
+    uri: process.env.REACT_APP_APOLLO_CLIENT_ENDPOINT,
+    clientState: {
+      defaults: { isEditMode: false },
+      resolvers: {}
+    }
+  })
+
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+  )
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
